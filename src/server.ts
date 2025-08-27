9a31d92a2f073ebe1f6a032278a8e0ead2b0796e
JulioCesar-A <juliocesar02e@gmail.com>
2025-08-27 15:19:15 -0300

    Criação de Base de Dados

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,25 @@
-import Fastify from 'fastify';
+interface DriverParams {
+    id: string
+}
 
-const fastify = Fastify({
-    logger: true
-});
+const teams = [
+  { id: 1, name: "McLaren", base: "Woking, United Kingdom" },
+  { id: 2, name: "Mercedes", base: "Brackley, United Kingdom" },
+  { id: 3, name: "Red Bull Racing", base: "Milton Keynes, United Kingdom" },
+  { id: 4, name: "Ferrari", base: "Maranello, Italy" },
+  { id: 5, name: "Alpine", base: "Enstone, United Kingdom" },
+  { id: 6, name: "Aston Martin", base: "Silverstone, United Kingdom" },
+  { id: 7, name: "Alfa Romeo Racing", base: "Hinwil, Switzerland" },
+  { id: 8, name: "AlphaTauri", base: "Faenza, Italy" },
+  { id: 9, name: "Williams", base: "Grove, United Kingdom" },
+  { id: 10, name: "Haas", base: "Kannapolis, United States" },
+  { id: 11, name: "Uralkali Haas F1 Team", base: "Banbury, United Kingdom" },
+  { id: 12, name: "Scuderia Toro Rosso", base: "Faenza, Italy" },
+];
 
-const port = process.env.PORT;
-
-console.log(port);
-console.log(fastify);
+const drivers = [
+  { id: 1, name: "Max Verstappen", team: "Red Bull Racing" },
+  { id: 2, name: "Lewis Hamilton", team: "Ferrari" },
+  { id: 2, name: "Lando Norris", team: "McLaren" },    
+];
 
