49f0003edc60552398d99f33edb4e2a78ac85790
JulioCesar-A <juliocesar02e@gmail.com>
2025-08-27 15:41:58 -0300

    Implementação de métodos Get

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,10 @@ interface DriverParams {
     id: string
 }
 
+interface TeamParams {
+    id: string
+}
+
 const teams = [
   { id: 1, name: "McLaren", base: "Woking, United Kingdom" },
   { id: 2, name: "Mercedes", base: "Brackley, United Kingdom" },
@@ -27,10 +31,61 @@ const teams = [
 ];
 
 const drivers = [
-  { id: 1, name: "Max Verstappen", team: "Red Bull Racing" },
-  { id: 2, name: "Lewis Hamilton", team: "Ferrari" },
-  { id: 2, name: "Lando Norris", team: "McLaren" },    
+  { id: 1, name: "Max Verstappen", teamId: 3 },
+  { id: 2, name: "Lewis Hamilton", teamId: 4 },
+  { id: 3, name: "Lando Norris", teamId: 1 },
 ];
+
+server.get('/teams', async(request, response) => {
+    response.type('application/json').code(200);
+    
+    return { teams };
+});
+
+server.get<{ Params: TeamParams }>('/teams/:id', async (request, response) => {
+    const id = parseInt(request.params.id);
+    const team = teams.find(t => t.id === id);
+
+    if (!team) {
+        response.type('application/json').code(404);
+        return { message: "Team Not Found" };
+    }
+
+    response.type('application/json').code(200);
+    return { team };
+});
+
+server.get<{ Params: TeamParams }>('/teams/:id/drivers', async (request, response) => {
+    const teamId = parseInt(request.params.id);
+    const teamDrivers = drivers.filter(driver => driver.teamId === teamId);
+    
+
+    if (teamDrivers.length === 0) {
+        response.type('application/json').code(404);
+        return { message: "No Drivers Found for this Team" };
+    }
+    response.type('application/json').code(200);
+    return { team: teams.find(team => team.id === teamId), drivers: teamDrivers };
+});
+
+server.get('/drivers', async(request, response) => {
+    response.type('application/json').code(200);
+
+    return { drivers };
+});
+
+server.get<{ Params: DriverParams }>('/drivers/:id', async(request, response) => {
+    const id = parseInt(request.params.id);
+    const driver = drivers.find(d => d.id === id);
+
+    if(!driver){
+        response.type('application/json').code(404);
+        return { message : "Driver Not Found"}
+    } else {
+        response.type('application/json').code(404);
+        return { driver }
+    }
+});
 
 server.listen({port: Number(portServer)}, () => {
     console.log("Server init"); 
